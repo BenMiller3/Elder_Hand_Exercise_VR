e9682ba217df531aab657430ac2d3d85432a6de7
Ben Miller <benjaminmiller229@gmail.com>
2016-10-14 19:48:36 -0800

    Update ElderExercise.py

--- ElderExercise.py
+++ ElderExercise.py
@@ -8,7 +8,12 @@ import sys, thread, time
 import Leap 
 from Leap import *
 
-
-
+"""
+Main leap class
+"""
+class LeapMotionListener(Leap.listener):
+  fingerNames = ["pinky","index","middle","ring","thumb"]
+  
+  
 def __main__():
   return 0
